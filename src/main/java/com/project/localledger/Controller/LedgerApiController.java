a47be6126831564e1024be3632e376a14d3030b4
jihwan <hwangjihwan4031@gmail.com>
2025-06-17 18:05:05 +0900

    달력 날짜 클릭하면 세부내역 표시

--- src/main/java/com/project/localledger/Controller/LedgerApiController.java
+++ src/main/java/com/project/localledger/Controller/LedgerApiController.java
@@ -5,18 +5,17 @@ import com.project.localledger.entity.AddIncome;
 import com.project.localledger.repository.ExpenseRepository;
 import com.project.localledger.repository.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/api/ledger")
 public class LedgerApiController {
 
@@ -30,12 +29,13 @@ public class LedgerApiController {
     }
 
     @GetMapping("/monthly")
+    @ResponseBody
     public Map<String, List<?>> getMonthlyLedger(
             @RequestParam int year,
             @RequestParam int month){
 
-        LocalDateTime startDate = LocalDate.of(year, month, 1).atStartOfDay();
-        LocalDateTime endDate = LocalDate.of(year, month, 1).withDayOfMonth(LocalDate.of(year, month, 1).lengthOfMonth()).atTime(23, 59, 59);
+        LocalDateTime startDate = LocalDate.of(year, month, 1).atStartOfDay(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        LocalDateTime endDate = LocalDate.of(year, month, 1).withDayOfMonth(LocalDate.of(year, month, 1).lengthOfMonth()).atTime(23, 59, 59).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
 
         List<AddIncome> incomes = incomeRepository.findByIncomeDateBetween(startDate, endDate);
         List<AddExpense> expenses = expenseRepository.findByExpenseDateBetween(startDate, endDate);
@@ -47,11 +47,12 @@ public class LedgerApiController {
     }
 
     @GetMapping("/daily")
+    @ResponseBody
     public Map<String, List<?>> getDailyLedger(
             @RequestParam LocalDate date) {
 
-        LocalDateTime startOfDay = date.atStartOfDay();
-        LocalDateTime endOfDay = date.atTime(23, 59, 59);
+        LocalDateTime startOfDay = date.atStartOfDay(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+        LocalDateTime endOfDay = date.atTime(23, 59, 59).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
 
         List<AddIncome> incomes = incomeRepository.findByIncomeDateBetween(startOfDay, endOfDay);
         List<AddExpense> expenses = expenseRepository.findByExpenseDateBetween(startOfDay, endOfDay);
